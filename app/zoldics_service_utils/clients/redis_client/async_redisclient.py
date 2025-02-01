079dd6182393c65a52ae9ea0039cf9a01275cab0
Nabarka Deb <nabarka.deb@u-next.com>
2025-02-01 23:16:20 +0530

    Added  changes  ...

--- app/zoldics_service_utils/clients/redis_client/async_redisclient.py
+++ app/zoldics_service_utils/clients/redis_client/async_redisclient.py
@@ -1,7 +1,7 @@
 import asyncio
 from redis.asyncio import Redis
 import json
-from typing import AsyncGenerator, Optional
+from typing import AsyncGenerator, Optional, Any, List, cast
 
 from ...interfaces.interfaces_pd import SSEPaylaod_PM
 from ...ioc.singleton import SingletonMeta
@@ -12,6 +12,24 @@ class AsyncRedisClient(metaclass=SingletonMeta):
         if not hasattr(self, "redis") and isinstance(redis_connection, Redis):
             self.redis: Redis = redis_connection
         self.pubsub = self.redis.pubsub()
+
+    async def set(self, key: str, value: Any, expiry: Optional[int] = None) -> bool:
+        return bool(await self.redis.set(key, value, ex=expiry))
+
+    async def get(self, key: str) -> Optional[bytes]:
+        return await self.redis.get(key)
+
+    async def delete(self, key: str) -> int:
+        return await self.redis.delete(key)
+
+    async def delete_all_keys(self, keys: List[str]) -> int:
+        return await self.redis.delete(*keys)
+
+    async def get_keys(self, pattern: str = "*") -> List[str]:
+        return cast(List[str], await self.redis.keys(pattern))
+
+    async def send_command(self, *commands: str) -> Any:
+        return await self.redis.execute_command(*commands)
 
     async def publish(self, channel: str, message: SSEPaylaod_PM) -> int:
         return await self.redis.publish(
@@ -39,4 +57,5 @@ class AsyncRedisClient(metaclass=SingletonMeta):
             await self.unsubscribe(channel=channel)
 
     async def close_connection(self) -> None:
+        await self.pubsub.close()
         await self.redis.close()
